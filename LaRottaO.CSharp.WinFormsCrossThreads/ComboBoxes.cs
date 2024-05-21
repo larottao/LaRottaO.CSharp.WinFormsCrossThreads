b43d2924d5bc12c8fa0a33c6a7fa9fc5a51e979f
larottao <larottao@gmail.com>
2024-05-21 15:26:44 -0600

    Added support from ComboBoxes

---
+++ LaRottaO.CSharp.WinFormsCrossThreads/ComboBoxes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LaRottaO.CSharp.WinFormsCrossThreads
+{
+    public static class ComboBoxes
+    {
+        public static String GetTextFromAnotherThread(this ComboBox comboBox)
+        {
+            String text = "";
+
+            if (Thread.CurrentThread.IsBackground)
+            {
+                comboBox.Invoke(new Action(() =>
+                {
+                    text = comboBox.Text;
+                }));
+            }
+            else
+            {
+                text = comboBox.Text;
+            }
+
+            return text;
+        }
+    }
+}
